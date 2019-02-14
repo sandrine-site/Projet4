c95354bdcd19e8d52e18212458be65d98c586c8f
slashcreations <ama.cloitre@free.fr>
2019-02-14 16:26:46 +0100

    Toutes les modifications ont eu lieux

---
+++ config/Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sandrine
+ * Date: 12/02/2019
+ * Time: 18:47
+ */
+return array(
+'url'=>'http://localhost/Projet4/index.php',
+'hostname' =>'mysql:host=localhost;dbname=slash1040766_1oujev;charset=utf8',
+'user'=>'root',
+'password'=> '',
+'error add comment'=>false,
+    'signal comment'=>false,
+    'password message'=>false,
+    'newPwerror'=>false,
+    'newPwless'=>false,
+    'message new Pw'=>'no',
+    "delete comment"=>false,
+);
--- view/backend/headerBack.php
+++ view/backend/headerBack.php
@@ -2,6 +2,8 @@
     <nav>
         <h1>Accés rapides</h1>
         <?php
+        $config=include('config/config.php');
+        $url=$config['url'];
         $active1=$active2=$active3=$active4='';
         switch( $title ){
             case 'Création des chapitres': $active1='active';
@@ -14,28 +16,77 @@
                 break;}
         ?>
         <div class="btn-group btn-group-justified" role="group" aria-label="...">
-            <a class="btn btn-primary <?= $active1?> " href="http://localhost/Projet4/index.php?action=<?='CreeNewChapter'?>" role="button">
+
+            <a class="btn btn-primary <?= $active1?> " href="<?=$url?>?action=<?='CreeNewChapter'?>" role="button">
                 <h5>Créer un chapitre</h5> <i class="fas fa-pen-nib"></i>
             </a>
-            <a class="btn btn-primary <?= $active2?>" href="http://localhost/Projet4/index.php?action=<?='ListOfChapter'?>" role="button">
+            <a class="btn btn-primary <?= $active2?>" href="<?=$url?>?action=<?='ListOfChapter'?>" role="button">
                 <h5>Editer un chapitre</h5> <i class="far fa-edit"></i>
             </a>
-            <a class="btn btn-primary <?= $active3?>" href="http://localhost/Projet4/index.php?action=<?='AllComments'?>&&message=" role="button">
+            <a class="btn btn-primary <?= $active3?>" href=<?=$url?>?action=<?='AllComments'?> role="button">
                 <h5>Commentaires</h5><i class="fas fa-list-ul"></i>
             </a>
-            <a class="btn btn-primary <?= $active4?>" href="http://localhost/Projet4/index.php?action=<?='Adminpw'?>&&message=" role="button">
+            <a class="btn btn-primary <?= $active4?>" href="<?=$url?>?action=<?='Adminpw'?>" role="button">
                 <h5> Mot de passe</h5><i class="fas fa-key"></i>
             </a>
-            <a class="btn btn-primary" href="http://localhost/Projet4/index.php" role="button">
+            <a class="btn btn-primary" href="<?=$url?>" role="button">
                 <h5>Quitter</h5> <i class="fas fa-sign-out-alt"></i>
             </a>
         </div>
-        <div class="warning">
+
+        <?php
+
+        if (isset($_SESSION["delete comment"])&& $_SESSION["delete comment"]==true)
+        {
+            ?>
+            <div class="warning">
+                <?=$_SESSION["delete comment"]?>
+            </div>
+
             <?php
-    if (isset($message)){
-        echo ($message);
-    }
+        }
+        elseif (isset($_SESSION['newPwerror'])&&$_SESSION['newPwerror']==true )
+        {
+            ?>
+            <div class="warning">
+                Attention le nouveau mot de passe et sa confirmation ne correspondent pas !
+            </div>
+
+            <?php
+        }
+        elseif (isset( $_SESSION['newPwless'])&& $_SESSION['newPwless']==true )
+        {
+            ?>
+            <div class="warning">
+                Attention tous les champs doivent être remplis !
+            </div>
+
+            <?php
+        }
+        elseif (isset( $_SESSION["message new Pw"])&& $_SESSION["message new Pw"]=='ko' )
+        {
+            ?>
+            <div class="warning">
+                Attention l'ancien mot de passe ne correspond pas !
+            </div>
+
+            <?php
+        }
+        elseif (isset( $_SESSION["message new Pw"])&& $_SESSION["message new Pw"]=='ok'){
+            ?>
+            <div class="thank">
+                Le mot de passe a bien été modifié !
+            </div>
+            <?php
+        }
+        elseif (isset( $_SESSION['doublons'])&& $_SESSION['doublons']==true ){
         ?>
+
+        <div class="warning">
+            attention vous avez deux fois le même numéro de chapitre
         </div>
+        <?php
+        }
+            ?>
     </nav>
 </header>
